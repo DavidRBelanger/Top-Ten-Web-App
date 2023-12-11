9eb2c0109802aa33a7f578f8817d90942339fff6
DavidRBelanger <94973702+DavidRBelanger@users.noreply.github.com>
2023-12-10 23:52:11 -0500

    styles

--- public/app.js
+++ public/app.js
@@ -1,19 +1,52 @@
+
 document.addEventListener("DOMContentLoaded", event => {
     
     const app = firebase.app();
-    console.log(app);
+
+    const db = firebase.firestore();
+
+    const list = db.collection('mainLists').doc("12-22-2023");
+    const tableElement = document.getElementById("myTable");
+    var nameList;
+    var valueList;
+    var title;
+    var source;
+
+    list.get().then((doc) => {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+            nameList = doc.data().itemNames;
+            valueList = doc.data().itemValues;
+            title = doc.data().title;
+            source = doc.data().source;
+
+            const titleElement = document.getElementById('listTitle');
+            titleElement.textContent = title;
+
+            const sourceElement = document.getElementById('source-text');
+            sourceElement.textContent = source;
+            console.log(source);
+            for (let i = 0; i < nameList.length; i++) {
+                const rowElement = document.createElement("tr");
+                const nameCellElement = document.createElement("td");
+                const valueCellElement = document.createElement("td");
+              
+                nameCellElement.textContent = nameList[i];
+                valueCellElement.textContent = valueList[i];
+                
+                rowElement.appendChild(nameCellElement);
+                rowElement.appendChild(valueCellElement);
+                
+                tableElement.appendChild(rowElement);
+              }
+        } else {
+            console.log("No Such Document!");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
 });
 
-function googleLogin() {
-    const provider = new firebase.auth.GoogleAuthProvider();
 
-    firebase.auth().signInWithPopup(provider)
-
-        .then(result => {
-            const user = result.user;
-            document.write(`Hello ${user.displayName}`);
-            console.log(user)
-        })
-        .catch(console.log)
-
-}
+    
+    
